e8bd71c3b1d1276ae25e9d6ccfa66e96f3a97239
Sadissou Chefik Olabissi Ichola <300130566@monboreal.ca>
2024-10-20 22:08:31 -0400

    2eme mis a jour

--- ConsoleAppLivres/Program.cs
+++ ConsoleAppLivres/Program.cs
@@ -8,10 +8,18 @@ internal class Program
         Revue revue1 = new Revue("Cybersecurite", "Robert Milan", 300, 2, 2015);
         Revue revue2 = new Revue("Cloud Computing", "Jean Michel", 200, 5, 2023);
         Revue revue3 = new Revue("Reseautique", "Raschad Gigan", 150, 3, 2017);
-          
+
+        Roman roman1 = new Roman("1984", "George Orwell", 328, "Science-fiction");
+        Roman roman2 = new Roman("Le Petit Prince", "Antoine de Saint-Exupery", 96, "Conte philosophique");
+        Roman roman3 = new Roman("La Peste", "albert Camus", 320, "Roman philosophique");
+
         revue1.AfficherDetails();
         revue2.AfficherDetails();
         revue3.AfficherDetails();
+
+        roman1.AfficherDetails();
+        roman2.AfficherDetails();
+        roman3.AfficherDetails();
     }
 
 }
---
+++ ConsoleAppLivres/Roman.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLivres
+{
+    internal class Roman : Livres
+    {
+        public string Genre { get; set; }
+
+        public Roman(string titre, string auteur, int nombrePages, string genre)
+            : base(titre, auteur, nombrePages)
+        {
+            Genre = genre;
+        }
+
+        public override void AfficherDetails()
+        {
+            base.AfficherDetails();
+            Console.WriteLine($"Genre: {Genre}");
+        }
+    }
+}
+
